bfd8a34b992fa03e94c677fa4862251ebdde674d
aksoff <an_popoff@inbox.ru>
2021-05-06 17:30:13 +0400

    dropdown menu

---
+++ #src/js/files/script.js
@@ -0,0 +1,27 @@
+//== Show/Hide Dropdown menu =================================
+const elem = document.querySelector('#dropdown-menu');
+elem.classList.toggle("scale-out-tl")
+
+let btn = document.querySelector('#btn-category');
+
+btn.onmouseover = (event) => {
+  if (elem.classList.contains("scale-out-tl")) {
+    menuShow();
+  } else {
+    menuHide();
+  }
+};
+
+btn.onmouseout = (event) => {
+  menuHide();
+};
+
+function menuShow() {
+    elem.classList.remove("scale-out-tl")
+    elem.classList.toggle("scale-in-tl")
+};
+
+function menuHide() {
+    elem.classList.remove("scale-in-tl")
+    elem.classList.toggle("scale-out-tl")
+}
--- klimat-c/js/app.js
+++ klimat-c/js/app.js
@@ -1,7 +1,33 @@
 function email_test(input) {
 	return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
 }
+//== Show/Hide Dropdown menu =================================
+const elem = document.querySelector('#dropdown-menu');
+elem.classList.toggle("scale-out-tl")
 
+let btn = document.querySelector('#btn-category');
+
+btn.onmouseover = (event) => {
+  if (elem.classList.contains("scale-out-tl")) {
+    menuShow();
+  } else {
+    menuHide();
+  }
+};
+
+btn.onmouseout = (event) => {
+  menuHide();
+};
+
+function menuShow() {
+    elem.classList.remove("scale-out-tl")
+    elem.classList.toggle("scale-in-tl")
+};
+
+function menuHide() {
+    elem.classList.remove("scale-in-tl")
+    elem.classList.toggle("scale-out-tl")
+}
 var ua = window.navigator.userAgent;
 var msie = ua.indexOf("MSIE ");
 var isMobile = { Android: function () { return navigator.userAgent.match(/Android/i); }, BlackBerry: function () { return navigator.userAgent.match(/BlackBerry/i); }, iOS: function () { return navigator.userAgent.match(/iPhone|iPad|iPod/i); }, Opera: function () { return navigator.userAgent.match(/Opera Mini/i); }, Windows: function () { return navigator.userAgent.match(/IEMobile/i); }, any: function () { return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows()); } };
